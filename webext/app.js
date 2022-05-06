2a3734c3624e9d36d46caf9adbabf50e8eba7c12
Alex6 <26258257+AL3X-69@users.noreply.github.com>
2022-05-06 09:37:43 +0200

    Remove debug logging

--- webext/app.js
+++ webext/app.js
@@ -2,18 +2,15 @@ const rickrollURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 let alertMessage = "⚠ You are about to get Rickroll. ⚠\n\nExit ?"
 
 window.addEventListener("load", () => {
-    console.log(window.location.href)
     if(window.location.href === rickrollURL){
         document.querySelectorAll('audio, video').forEach(item => {
             item.muted = true;
             item.pause();
         });
         if(confirm(alertMessage) === true){
-            console.log("Rickroll denied");
             open('https://www.youtube.com/', '_self', '');
         }
         else {
-            console.log("Rickroll accepted");
             document.querySelectorAll('audio, video').forEach(item => {
                 item.muted = false;
                 item.play();
